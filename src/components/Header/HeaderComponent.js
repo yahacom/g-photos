a9c6e579584b4f190ca261c06be78534c321254a
Yakiv Vakoliuk <yahacomua@gmail.com>
2020-08-04 20:27:42 +0300

    Add info to Header component, change styles

--- src/components/Header/HeaderComponent.js
+++ src/components/Header/HeaderComponent.js
@@ -3,13 +3,17 @@ import styles from './styles.module.scss';
 import {authActions} from '../../ducks/auth';
 import {store} from '../../store';
 
-const HeaderComponent = () => {
+const HeaderComponent = ({title, user, email}) => {
   const logOut = (e) => {
     e.preventDefault();
     store.dispatch(authActions.signOut());
   }
   return (
     <div className={styles.header}>
+      <div className={styles.infoWrapper}>
+        <h1>{title}</h1>
+        <div className={styles.userInfo}>{user}<br />{email}</div>
+      </div>
       <a href='#' onClick={logOut}>Log Out</a>
     </div>
   )
