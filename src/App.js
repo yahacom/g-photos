f95ef8620ef703f73e6ea0472f5e552f3d8775a7
Yakiv Vakoliuk <yahacomua@gmail.com>
2020-08-04 15:35:52 +0300

    Add envs, load gapi

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import RootRouter from './router';
 
 function App() {
+  useEffect(() => {
+    window.gapi && window.gapi.load('auth2:client');
+  }, []);
   return (
     <Router>
       <RootRouter />
