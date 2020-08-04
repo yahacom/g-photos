2f4a0a813d938e1af80b3eb7f145ee7d0378f712
Yakiv Vakoliuk <yahacomua@gmail.com>
2020-08-04 16:04:12 +0300

    Connect store to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, {useEffect} from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import RootRouter from './router';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {store, persistor} from './store';
 
 function App() {
   useEffect(() => {
     window.gapi && window.gapi.load('auth2:client');
   }, []);
   return (
-    <Router>
-      <RootRouter />
-    </Router>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <RootRouter />
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 }
 
