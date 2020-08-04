689d2fa3be699bf389436318cdcd65543e6477e6
Yakiv Vakoliuk <yahacomua@gmail.com>
2020-08-04 20:15:50 +0300

    Add styles for albums

--- src/pages/Albums/AlbumsPage.js
+++ src/pages/Albums/AlbumsPage.js
@@ -1,5 +1,7 @@
 import React, {useEffect} from 'react';
 import Header from '../../components/Header';
+import {Link} from 'react-router-dom';
+import styles from './styles.module.scss';
 
 const AlbumsPage = ({loading, albums, isInitialized, getAlbums}) => {
   useEffect(() => {
@@ -7,10 +9,21 @@ const AlbumsPage = ({loading, albums, isInitialized, getAlbums}) => {
       getAlbums();
     }
   }, [isInitialized]);
+  const renderAlbums = () => albums.map(({id, title, coverPhotoBaseUrl, mediaItemsCount}) => (
+    <Link to={`/albums/${id}`} className={styles.item} key={id}>
+      <div className={styles.itemCover} style={{backgroundImage: `url(${coverPhotoBaseUrl}`}} />
+      <div className={styles.itemInfo}>
+        <b>{title}</b><br />
+        {mediaItemsCount} photos inside album
+      </div>
+    </Link>
+  ))
   return (
-    <div>
+    <div className={styles.albums}>
       <Header/>
-      <h1>Albums list</h1>
+      <div className={styles.albumsList}>
+        {renderAlbums()}
+      </div>
     </div>
   )
 };
