a8fd526d7afddae42a90e95d46cc0a459c2082a4
Yakiv Vakoliuk <yahacomua@gmail.com>
2020-08-04 21:45:07 +0300

    Connect Photos to store, add layout and styles

--- src/pages/Photos/PhotosPage.js
+++ src/pages/Photos/PhotosPage.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {Header, Loader} from '../../components'
+import styles from './styles.module.scss';
+import {useParams} from 'react-router-dom'
 
-const PhotosPage = () => {
+const PhotosPage = ({loading, photos, isInitialized, getPhotos, name, email}) => {
+  const {albumId} = useParams();
+  useEffect(() => {
+    if (isInitialized) {
+      getPhotos({albumId});
+    }
+  }, [isInitialized]);
+  const renderPhotos = () => photos.map(({id, baseUrl}) => (
+    <div className={styles.item} key={id}>
+      <div className={styles.itemCover} style={{backgroundImage: `url(${baseUrl}`}} />
+    </div>
+  ))
   return (
-    <h1>Photos list</h1>
+    <div className={styles.albums}>
+      <Header
+        title='Photos list'
+        user={name}
+        email={email} />
+      <div className={styles.albumsList}>
+        {renderPhotos()}
+      </div>
+      {loading && <Loader />}
+    </div>
   )
 };
 
--- src/pages/Photos/index.js
+++ src/pages/Photos/index.js
@@ -1,1 +1,22 @@
-export {default} from './PhotosPage';
+import {connect} from 'react-redux';
+import PhotosPage from './PhotosPage'
+import {albumsActions} from '../../ducks/albums';
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    loading: state.albums.loading || !state.initialize.isInitialized,
+    photos: state.albums.photos,
+    isInitialized: state.initialize.isInitialized,
+    name: state.auth.name,
+    email: state.auth.email,
+  };
+};
+
+const mapDispatchToProps = {
+  getPhotos: albumsActions.photosRequest,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(PhotosPage);
