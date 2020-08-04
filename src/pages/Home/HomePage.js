380c6f8b3fd06cf6c6df112ea7d649a8823d058a
Yakiv Vakoliuk <yahacomua@gmail.com>
2020-08-04 15:44:41 +0300

    Testing Google Auth

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -3,8 +3,19 @@ import styles from './styles.module.scss';
 import gPhotosIcon from '../../assets/icon_google_photos.svg';
 
 const HomeScreen = () => {
+  const signIn = async () => {
+    const {gapi} = window;
+    const authInstance = await gapi.auth2.getAuthInstance();
+    return await authInstance.signIn();
+  }
   const onClick = () => {
-
+    const {gapi} = window;
+    gapi.client.init({
+      apiKey: process.env.REACT_APP_API_KEY,
+      client_id: process.env.REACT_APP_CLIENT_ID,
+      scope: 'https://www.googleapis.com/auth/photoslibrary'
+    });
+    console.log(signIn());
   };
   return (
     <div className={styles.home}>
